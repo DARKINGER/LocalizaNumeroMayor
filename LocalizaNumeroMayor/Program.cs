04ffcace95b65c2ff95aee4e2e4cfb235a88145e
DARKINGER <alex10200110@gmail.com>
2020-12-02 07:53:31 -0600

    Primer commit
    codigo para conocer la casilla con el valor mayor

---
+++ LocalizaNumeroMayor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LocalizaNumeroMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Favor de ingresar el tamaño del arreglo");
+            int[] arreglo = new int[int.Parse(Console.ReadLine())];
+            
+            for (int x =0; x < arreglo.Length; x++)
+            {
+                Console.WriteLine("Inrese el valor de la casilla "+x);
+                 arreglo[x] = int.Parse(Console.ReadLine());
+            }
+            //
+            Console.WriteLine("El arreglo es:\n");
+            for (int x = 0; x<arreglo.Length; x++)
+            {
+                Console.WriteLine(arreglo[x] );
+            }
+
+            Console.WriteLine("\nLa casilla con el valor mas grande es: "+SacarMayor(arreglo).ToString());
+
+            Console.ReadKey();
+        }
+
+        static int SacarMayor(int[] arreglo)
+        {
+            int CasillaMayor = 0, ValorMayor = 0;
+            for (int x = 0; x < arreglo.Length; x++)
+            {
+                if (x == 0)
+                {
+                    CasillaMayor = x;
+                    ValorMayor = arreglo[x];
+                }
+                else
+                {
+                    if (arreglo[x] >= ValorMayor)
+                    {
+                        if (arreglo[x] > ValorMayor)
+                        {
+                            CasillaMayor = x;
+                            ValorMayor = arreglo[x];
+                        }
+                    }
+                }
+            }
+            return CasillaMayor;
+        }
+
+    }
+}
